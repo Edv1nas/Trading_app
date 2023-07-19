0e41d90e0b09d85e86aff7579640bf7c537099e6
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-07-19 18:12:41 +0300

    back end code

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import Depends, FastAPI
+
+
+from router.router import api_router
+
+from database.base import Base
+from database.db import engine
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.include_router(api_router, prefix="/api/v1")
